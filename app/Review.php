fddc51a2d70b1f3a3114ad4e5475ba400b4b1e8d
maarten079 <m.moerland.92@gmail.com>
2019-03-12 10:09:32 +0100

    added: relationship many-to-one and one-to-many between review and journey. database constraints regarding foreign key.

--- app/Review.php
+++ app/Review.php
@@ -8,4 +8,9 @@ class Review extends Model
 {
     // Auto generation of timestamps for created_at and updated_at is forced.
     public $timestamps = true;
+
+    public function journey()
+    {
+        return $this->hasOne('App\Journey');
+    }
 }
---
+++ database/migrations/2019_03_12_083823_create_journeys_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJourneysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('journeys', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+
+            // Foreign key constraint definition for database.
+            $table->unsignedInteger('review');
+            $table->foreign('review')->references('id')->on('reviews')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('journeys');
+    }
+}
