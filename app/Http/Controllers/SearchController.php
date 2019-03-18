8c282041ad2753628bc39494dea1f731882677d9
maarten079 <m.moerland.92@gmail.com>
2019-03-18 15:18:16 +0100

    updated to work with cloud and updated filters

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -8,13 +8,7 @@ use App\Review;
 class SearchController extends Controller
 {
     public function filter(Request $request, Review $reviews){
-        $id = $request->input('id');
-        $message = $request->input('message');
-        $date = $request->input('date');
-        $rating = $request->input('rating');
-        $vehicle = $request->input('vehicle');
-
-        $reviews = Review::where('id', 'like', '%' . $id . '%')->where('message', 'like', '%' . $message . '%')->where('created_at', 'like', '%' . $date . '%')->where('rating', 'like', '%' . $rating . '%')->where('vehicle', 'like', '%' . $vehicle . '%')->get();
+        $reviews = Review::where('id', 'like', '%' . $request->input('id') . '%')->where('message', 'like', '%' . $request->input('message') . '%')->where('created_at', 'like', '%' . $request->input('date') . '%')->where('rating', 'like', '%' . $request->input('rating') . '%')->where('vehicle_id', 'like', '%' . $request->input('vehicle') . '%')->get();
 
         return view('reviews.index')->with('reviews', $reviews);
         }
--- resources/views/reviews/index.blade.php
+++ resources/views/reviews/index.blade.php
@@ -1,58 +1,64 @@
-@extends('layouts.app')
-
+@extends('layouts.app') 
 @section('content')
-    <h1>
-        Latest reviews
-    </h1>
-    <p>
-        <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseFilters" aria-expanded="false" aria-controls="collapseFilters">
+<h1>
+    Latest reviews
+</h1>
+<p>
+    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseFilters" aria-expanded="false"
+        aria-controls="collapseFilters">
         Filters
         </button>
-    </p>
+</p>
 
-    <div class="collapse mb-3" id="collapseFilters">
-        <div class="card card-body">
-            <form action="{{url('/search')}}" method="POST" class="mb-3">
-                @csrf
-                <div class="form-group">
-                    <label for="idInput">id:</label>
-                    <input class="form-control mb-3" id="idInput" name="id" placeholder="review id">
-                </div>
-                <div class="form-group">
-                    <label for="messageInput">message:</label>
-                    <input class="form-control mb-3" id="messageInput" type="text" name="message" placeholder="review message">
-                </div>
-                <div class="form-group">
-                    <label for="dateInput">date:</label>
-                    <input class="form-control mb-3" id="dateInput" type="date" name="date">
-                </div>
-                <div class="form-group">
-                    <label for="vehicleInput">vehicle:</label>
-                    <input class="form-control mb-3" id="vehicleInput" type="text" name="vehicle" placeholder="vehicle number">
-                </div>
-                <div class="form-group">
-                    <label for="ratingInput">rating:</label>
-                    <select class="form-control mb-3" id="ratingInput" name="rating">
+<div class="collapse mb-3" id="collapseFilters">
+    <div class="card card-body">
+        <form action="{{url('/search')}}" method="POST" class="mb-3">
+            @csrf
+            <div class="form-group">
+                <label for="idInput">id:</label>
+                <input class="form-control mb-3" id="idInput" name="id" placeholder="review id">
+            </div>
+            <div class="form-group">
+                <label for="messageInput">message:</label>
+                <input class="form-control mb-3" id="messageInput" type="text" name="message" placeholder="review message">
+            </div>
+            <div class="form-group">
+                <label for="dateInput">date:</label>
+                <input class="form-control mb-3" id="dateInput" type="date" name="date">
+            </div>
+            <div class="form-group">
+                <label for="vehicleInput">vehicle:</label>
+                <input class="form-control mb-3" id="vehicleInput" type="text" name="vehicle" placeholder="vehicle number">
+            </div>
+            <div class="form-group">
+                <label for="ratingInput">rating:</label>
+                <select class="form-control mb-3" id="ratingInput" name="rating">
                         <option></option>
                         <option>Good</option>
                         <option>Neutral</option>
                         <option>Bad</option>
                     </select>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-        </div>
-    </div>
-    
-    @if(count($reviews) > 0)
-        @foreach($reviews as $review)
-            <div class="well">
-                <a href="/reviews/{{$review->id}}">{{$review->id}}</a></h3>
-                <small>Created on {{$review->created_at}}</small>
-                <hr>
             </div>
-        @endforeach
-    @else
-        <p>No reviews found</p>
-    @endif
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</div>
+
+@if(count($reviews) > 0) @foreach($reviews as $review)
+<div class="well">
+    <a href="/reviews/{{$review->id}}">{{$review->id}}</a></h3>
+    <small>Created on {{$review->created_at}}</small>
+    <br>
+    <small>rating: {{$review->rating}}</small>
+    <br>
+    <small>img: {{$review->image_path}}</small>
+    <br>
+    <small>vehicle: {{$review->vehicle_id}}</small>
+    <br>
+    <small>coordinates: {{$review->coordinates}}</small>
+    <hr>
+</div>
+@endforeach @else
+<p>No reviews found</p>
+@endif
 @endsection
