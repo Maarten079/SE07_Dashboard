a433c7cd5e37f65127622bafcc7c003c6795d223
Loon, J.G. van (17056292) <17056292@student.hhs.nl>
2019-03-28 14:46:58 +0100

    working journy connection to vehicle

--- app database connection/adddata.php
+++ app database connection/adddata.php
@@ -1,22 +1,22 @@
 <?php
 include 'conn.php';
 
-$message= mysqli_real_escape_string($conn,  $_POST['message']);
+$message=mysqli_real_escape_string($conn,  $_POST['message']);
 $rating=mysqli_real_escape_string($conn, $_POST['rating']);
 $img_path='test';
 $vehicle_id=mysqli_real_escape_string($conn, $_POST['vehicle_id']);
 $lng=23.53;
 $lat=12.23;
 
-$r = mysqli_query($conn, "SELECT journeynumber 
+$journey_query = mysqli_query($conn, "SELECT id 
                           FROM `journeys`
                           WHERE vehicle_id = '$vehicle_id'
-                          AND journey_date <= '2019-03-11 04:57:47'
+                          AND journey_date <= CURRENT_TIMESTAMP
                           LIMIT 1");
 
-$journey_id = mysqli_fetch_array($r);
+$journey_row = mysqli_fetch_array($journey_query);
 
-$journey_id = (string)$journey_id;
+$journey_id = $journey_row['id'];
 
 $conn->query("insert into reviews(message, rating, img_path, journey_id, vehicle_id, lat, lng) values('".$message."','".$rating."','".$img_path."','".$journey_id."','".$vehicle_id."','".$lat."','".$lng."')");
 ?>
