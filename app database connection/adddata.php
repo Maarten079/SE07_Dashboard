7d2d81ba848590ee566d3bb9cfbdc3addf9f39d6
ImSythn <jgv.loon@gmail.com>
2019-03-28 12:23:40 +0100

    php mysqli scipt

--- app database connection/adddata.php
+++ app database connection/adddata.php
@@ -4,10 +4,19 @@ include 'conn.php';
 $message= mysqli_real_escape_string($conn,  $_POST['message']);
 $rating=mysqli_real_escape_string($conn, $_POST['rating']);
 $img_path='test';
-$journey_id=1;
-$vehicle_id=3116;
+$vehicle_id=mysqli_real_escape_string($conn, $_POST['vehicle_id']);
 $lng=23.53;
 $lat=12.23;
+
+$r = mysqli_query($conn, "SELECT journeynumber 
+                          FROM `journeys`
+                          WHERE vehicle_id = '$vehicle_id'
+                          AND journey_date <= '2019-03-11 04:57:47'
+                          LIMIT 1");
+
+$journey_id = mysqli_fetch_array($r);
+
+$journey_id = (string)$journey_id;
 
 $conn->query("insert into reviews(message, rating, img_path, journey_id, vehicle_id, lat, lng) values('".$message."','".$rating."','".$img_path."','".$journey_id."','".$vehicle_id."','".$lat."','".$lng."')");
 ?>
