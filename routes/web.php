58ae794a3596b0058feef39b14dcde81d68f52b3
maarten079 <m.moerland.92@gmail.com>
2019-04-04 16:32:14 +0200

    added: picture storage

--- routes/web.php
+++ routes/web.php
@@ -36,20 +36,26 @@ Route::post('/search-journeys', 'SearchController@filterJourneys');
 Route::post('/search-reviews-for-map', 'SearchController@filterReviewsForMap');
 
 Route::post('/review', function(Request $request){
-  //dd($request);
-
   // Create new review object
   $review = new Review;
 
   $review->message = $request->input('message');
   $review->rating = $request->input('rating');
-  $review->img_path = $request->input('img_path');
   $review->vehicle_id = $request->input('vehicle_id');
   $review->lng = $request->input('lng');
   $review->lat = $request->input('lat');
 
   $journey = Journey::where('journey_date', '<=', Carbon::now())->where('vehicle_id', $request->input('vehicle_id'))->firstOrFail();
+
   $review->journey_id = $journey->id;
+
+  $image = $request->input('img_path');  // your base64 encoded
+  $image = str_replace('data:image/png;base64,', '', $image);
+  $image = str_replace(' ', '+', $image);
+  $imageName = str_random(10).'.'.'png';
+  File::put(storage_path(). '/' . $imageName, base64_decode($image));
+
+  $review->img_path = $imageName;
 
   $review->save();
 });
